6e4e92b574ba6570273bda01d8c924e7917fa572
jojohanb <130059240+jojohanb@users.noreply.github.com>
2024-05-12 15:52:01 -0300

    em andamento

---
+++ gerador/script.js
@@ -0,0 +1,23 @@
+var input = document.getElementById("input")
+var link = document.getElementById("link")
+var enviar = document.getElementById("enviar")
+
+
+input.addEventListener("input", validarTelefone);
+
+function validarTelefone(event) {
+    var telefone = event.target.value;
+    var validaTelefone = /^[\d()-\s]+$/;
+    var digitos = telefone.replace(/\D/g, "");
+
+    if (!validaTelefone.test(telefone)) {
+        alert("Preencha o campo corretamente com 11 dígitos!");
+        event.target.value = telefone.slice(0, -1);
+        return;
+    }
+    var telefoneFormatado = digitos.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+    event.target.value = telefoneFormatado;
+    event.target.style.color="green"
+
+}
+
