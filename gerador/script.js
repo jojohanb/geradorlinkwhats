f77ce8fac7b4ca320625c7d0a767e8d4fc08c818
jojohanb <130059240+jojohanb@users.noreply.github.com>
2024-05-14 23:21:22 -0300

    em andamento

--- gerador/script.js
+++ gerador/script.js
@@ -2,22 +2,29 @@ var input = document.getElementById("input")
 var link = document.getElementById("link")
 var enviar = document.getElementById("enviar")
 
+document.getElementById('link').addEventListener('click', function() {
+    var Input2 = document.getElementById('input2');
+    Input2.style.display = Input2.style.display === 'none' ? 'block' : 'none'; //
+});
+
 
 input.addEventListener("input", validarTelefone);
 
 function validarTelefone(event) {
     var telefone = event.target.value;
     var validaTelefone = /^[\d()-\s]+$/;
-    var digitos = telefone.replace(/\D/g, ""); 
+    var digitos = telefone.replace(/\D/g, "");  //
 
-    if (!validaTelefone.test(telefone))  { 
-        alert("Por Favor, preencha o campo corretamente com 11 dígitos!");
+    if (!validaTelefone.test(telefone))  {
         event.target.value = telefone.slice(0, -1);
         return;
+    } 
+    if (digitos.length===11){
+        event.target.style.color="green"
+    }  else {
+        event.target.style.color = "";
     }
     var telefoneFormatado = digitos.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
     event.target.value = telefoneFormatado;
-    event.target.style.color="green"
-
 }
 
