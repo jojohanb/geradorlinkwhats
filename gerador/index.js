ed6a14473936b62c4413ba458180c939637d8f24
jojohanb <130059240+jojohanb@users.noreply.github.com>
2024-05-16 02:25:00 -0300

    quase la

--- gerador/index.js
+++ gerador/index.js
@@ -1,19 +1,63 @@
-var copiar = document.getElementById("copiar")
-var areaLink = document.getElementById("area-link")
-var texto = document.getElementById("texto")
+//declarando as variaveis
 
-copiar.addEventListener("click", function(){
-  if (areaLink.style.display=="none"){
-      areaLink.style.display="inline"
-      texto.style.display="inline"
-  } else{
-    areaLink.style.display="none"
-    texto.style.display="none"
+var copiar = document.getElementById("copiar");
+var areaLink = document.getElementById("area-link");
+var texto = document.getElementById("texto");
+var inputTelefone = document.getElementById("input-telefone")
+var enviar = document.getElementById("enviar");
+
+//adicionando eventos
+inputTelefone.addEventListener("input", validarTelefone);
+// copiar.addEventListener("click", gerarLink);
+
+ copiar.addEventListener("click", function(){
+   if (areaLink.style.display=="none"){
+       areaLink.style.display="inline"
+       texto.style.display="inline"
+   } else{
+     areaLink.style.display="none"
+     texto.style.display="none"
+   }
+ });
+
+//funcoes
+
+function validarTelefone(event) {
+  var telefone = event.target.value;
+  var validaTelefone = /^[\d()-\s]+$/;
+  var digitos = telefone.replace(/\D/g, ""); //Essa linha tem o objetivo de remover todos os caracteres não numéricos
+
+  if (!validaTelefone.test(telefone))  {
+      event.target.value = telefone.slice(0, -1);
+      return;
+  } 
+
+  if (digitos.length <= 11) {
+    var telefoneFormatado = digitos.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+    event.target.value = telefoneFormatado
+  } 
+
+  if (digitos.length===11) {
+      event.target.style.color = "green";
+  } else {
+     event.target.style.color = ""
   }
-})
+}
 
 
+// function gerarLink() {
+//   var telefone = inputTelefone.value;
+//   var digitos = telefone.replace(/\D/g, ""); 
 
+//   if (digitos.length === 11) {
+//       var link = "https://wa.me/55" + digitos;
+//       areaLink.innerHTML = `<a href="${link}" target="_blank">${link}</a>`;
+//       areaLink.style.display = "inline";
+//       texto.style.display = "inline";
+//   } else {
+//       alert("Por favor, insira um número de telefone válido.");
+//   }
+// }
 
 
 
@@ -35,30 +79,6 @@ copiar.addEventListener("click", function(){
 //input2.addEventListener("input", copiarLink());
 //link.addEventListener("click", gerarLink())
 
-
-
-//function validarTelefone(event) {
-//    var telefone = event.target.value;
-  //  var validaTelefone = /^[\d()-\s]+$/;
-    //var digitos = telefone.replace(/\D/g, "");  //nessa linha o replace serve para substituir os nao digitos por vazio
-
-   // if (!validaTelefone.test(telefone))  {
-     //   event.target.value = telefone.slice(0, -1);
-      //  return;
-   // } 
-   // if (digitos.length===11){
-     //   event.target.style.color="green"
-   // }  else {
-    //    event.target.style.color = "";
-   // }
-   // var telefoneFormatado = digitos.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
-   // event.target.value = telefoneFormatado;
-//}
-
-//function gerarLink(){
-  //  var randomLink= Math.random().toString(36).slice(-10)
-    //input2.value=randomLink
-//}
 
 //function copiarLink(){
 //    navigator.clipboard.writeText(input.value).then(() => {
